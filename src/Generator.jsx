98f4cf5ade344dd6b3da91d4d838ce9ee7f0dbf6
KostenkoYaroslav <yaroslavkostenko34@gmail.com>
2024-04-12 21:22:43 +0300

    Deleted voice

--- src/Generator.jsx
+++ src/Generator.jsx
@@ -1,13 +1,10 @@
 import React, { useState } from "react";
 import "./generator.css";
-import { IoVolumeLowSharp } from "react-icons/io5";
 import { TbCopy } from "react-icons/tb";
 import { TiSocialTwitter } from "react-icons/ti";
 import axios from "axios";
-import { useSpeechSynthesis } from "react-speech-kit";
 
 export default function Generator() {
-  const { speak } = useSpeechSynthesis();
   const [quote, setQuote] = useState("");
   const [author, setAuthor] = useState("");
   const getQuote = () => {
@@ -35,12 +32,6 @@ export default function Generator() {
         <hr />
         <footer>
           <div>
-            <IoVolumeLowSharp
-              onClick={() => speak({ text: quote })}
-              size={40}
-              color="#0000fe"
-              className="icon"
-            />
             <TbCopy
               onClick={copyQuote}
               size={40}
